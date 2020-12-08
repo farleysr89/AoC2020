0cb20997224ac61c0f33021dd5a21361a83a8e56
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-08 08:16:24 -0600

    Day08 Part1 done

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -13,6 +13,34 @@ namespace Day08
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            string[] commands = _input.Split('\n');
+            bool[] visited = new bool[commands.Length];
+            int acc = 0;
+            int next = 0;
+            while (true)
+            {
+                if (visited[next]) break;
+                visited[next] = true;
+                string[] command = commands[next].Split(" ");
+                switch (command[0])
+                {
+                    case "acc":
+                        int i = int.Parse(command[1]);
+                        acc += i;
+                        next++;
+                        break;
+                    case "jmp":
+                        int x = int.Parse(command[1]);
+                        next += x;
+                        break;
+                    case "nop":
+                        next++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("accumulator = " + acc);
         }
         static void SolvePart2()
         {
