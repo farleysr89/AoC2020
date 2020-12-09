dce6a2be18bb38f6a3dd4b46a06f5b2c30429499
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-09 08:17:52 -0600

    Day09 initial commit, add link to day08 readme

---
+++ Day09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+        static void SolvePart1()
+        {
+            string _input = File.ReadAllText("Input.txt");
+        }
+        static void SolvePart2()
+        {
+            string _input = File.ReadAllText("Input.txt");
+        }
+    }
+}
