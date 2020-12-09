5c00e1a09dd2e11bc1fb661281132f774d724dfe
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-09 17:29:13 -0600

    Day09 Part1 done

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -15,7 +15,42 @@ namespace Day09
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
-            List<int> nums = _input.Split('\n').Select(x => int.Parse(x)).ToList();
+            long[] nums = _input.Split('\n').Select(x => long.Parse(x)).ToArray();
+            int index = 25;
+            int begin = 0;
+            bool found = false;
+            long n = 0;
+            while (!found)
+            {
+                n = nums[index];
+                bool legal = false;
+                foreach (long i in nums.Skip(begin).Take(25))
+                {
+                    int second = 1;
+                    foreach(long x in nums.Skip(begin + second).Take(25 - second))
+                    {
+                        if(i + x == n)
+                        {
+                            legal = true;
+                            break;
+                        }
+                    }
+                    if (legal)
+                    {
+                        break;
+                    }
+                    second++;
+                }
+                if (!legal)
+                {
+                    found = true;
+                    break;
+                }
+                index++;
+                begin++;
+
+            }
+            Console.WriteLine("Illegal number is " + n);
         }
         static void SolvePart2()
         {
