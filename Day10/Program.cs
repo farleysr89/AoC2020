c3a430c9d876468ee95a23222f057af4bf528322
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-10 17:08:19 -0600

    Day10 Part 2 Complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -35,11 +35,66 @@ namespace Day10
                     prev = i;
                 }
             }
-            Console.WriteLine("Solution is : " + ones * threes);
+            Console.WriteLine("Solution to part 1 is : " + ones * threes);
         }
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<int> data = _input.Split('\n').Select(s => int.Parse(s)).ToList();
+            data.Add(0);
+            data.Add(data.Max() + 3);
+            data.Sort();
+            int prev = -1;
+            List<List<int>> subGroups = new List<List<int>>();
+            List<int> sub = new List<int>();
+            foreach(int i in data)
+            {
+                if (prev == -1)
+                {
+                    prev = i;
+                    sub.Add(i);
+                }
+                else
+                {
+                    if(i - prev == 1)
+                    {
+                        sub.Add(i);
+                        prev = i;
+                    }
+                    else
+                    {
+                        subGroups.Add(sub);
+                        sub = new List<int>();
+                        sub.Add(i);
+                        prev = i;
+                    }
+                }
+            }
+            subGroups.Add(sub);
+            long answer = 1;
+            foreach(var s in subGroups)
+            {
+                if(s.Count == 1 || s.Count == 2)
+                {
+                    continue;
+                }
+                if(s.Count == 3)
+                {
+                    answer *= 2;
+                    continue;
+                }
+                if(s.Count == 4)
+                {
+                    answer *= 4;
+                    continue;
+                }
+                if(s.Count == 5)
+                {
+                    answer *= 7;
+                    continue;
+                }
+            }
+            Console.WriteLine("Solution to part 2 is " + answer);
         }
     }
 }
