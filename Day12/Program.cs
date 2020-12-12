7ecb042477e722911eae8d1b67dca833f6247348
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-12 09:33:04 -0600

    Day 12 Part 2 Complete

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -23,7 +23,7 @@ namespace Day12
             int x = 0;
             int y = 0;
             char curDir = 'E';
-            foreach(var s in data)
+            foreach (var s in data)
             {
                 char c = s[0];
                 int i = int.Parse(s[1..]);
@@ -41,7 +41,7 @@ namespace Day12
                 }
                 else if (turns.Contains(c))
                 {
-                    if(i == 180)
+                    if (i == 180)
                     {
                         if (curDir == 'E') curDir = 'W';
                         else if (curDir == 'W') curDir = 'E';
@@ -53,9 +53,9 @@ namespace Day12
                             break;
                         }
                     }
-                    else if(c == 'R')
+                    else if (c == 'R')
                     {
-                        if(i == 90)
+                        if (i == 90)
                         {
                             if (curDir == 'E') curDir = 'S';
                             else if (curDir == 'W') curDir = 'N';
@@ -67,7 +67,7 @@ namespace Day12
                                 break;
                             }
                         }
-                        else if(i == 270)
+                        else if (i == 270)
                         {
                             if (curDir == 'E') curDir = 'N';
                             else if (curDir == 'W') curDir = 'S';
@@ -122,7 +122,7 @@ namespace Day12
                         }
                     }
                 }
-                else if(c == forward)
+                else if (c == forward)
                 {
                     if (curDir == 'N')
                         x += i;
@@ -150,6 +150,94 @@ namespace Day12
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            char[] dir = new char[] { 'N', 'W', 'S', 'E' };
+            char[] turns = new char[] { 'L', 'R' };
+            char forward = 'F';
+            int x = 0;
+            int y = 0;
+            int wayX = 10;
+            int wayY = 1;
+            foreach (var s in data)
+            {
+                char c = s[0];
+                int i = int.Parse(s[1..]);
+                if (dir.Contains(c))
+                {
+                    if (c == 'N') wayY += i;
+                    else if (c == 'S') wayY -= i;
+                    else if (c == 'E') wayX += i;
+                    else if (c == 'W') wayX -= i;
+                    else
+                    {
+                        Console.WriteLine("Something Broke!");
+                        break;
+                    }
+                }
+                else if (turns.Contains(c))
+                {
+                    if (i == 180)
+                    {
+                        wayX = -wayX;
+                        wayY = -wayY;
+                    }
+                    else if (c == 'R')
+                    {
+                        if (i == 90)
+                        {
+                            int tmpX = wayX;
+                            int tmpY = wayY;
+                            wayX = tmpY;
+                            wayY = -tmpX;
+                        }
+                        else if (i == 270)
+                        {
+                            int tmpX = wayX;
+                            int tmpY = wayY;
+                            wayX = -tmpY;
+                            wayY = tmpX;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Something Broke!");
+                            break;
+                        }
+                    }
+                    else if (c == 'L')
+                    {
+                        if (i == 90)
+                        {
+                            int tmpX = wayX;
+                            int tmpY = wayY;
+                            wayX = -tmpY;
+                            wayY = tmpX;
+                        }
+                        else if (i == 270)
+                        {
+                            int tmpX = wayX;
+                            int tmpY = wayY;
+                            wayX = tmpY;
+                            wayY = -tmpX;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Something Broke!");
+                            break;
+                        }
+                    }
+                }
+                else if (c == forward)
+                {
+                    x += i * wayX;
+                    y += i * wayY;
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke!");
+                    break;
+                }
+            }
+            Console.WriteLine("Manhattan Distance " + (Math.Abs(x) + Math.Abs(y)));
         }
     }
 }
