12cbbe613f85efba29f5f8c3b7c19cf3a49e94b5
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-13 10:32:26 -0600

    Day 13 Part 2 Complete

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -26,12 +26,41 @@ namespace Day13
                 busLines.Add(new BusLine { id = int.Parse(s), goal = time });
             }
             var best = busLines.OrderBy(x => x.BoardingTime()).First();
-            Console.WriteLine("Solution = " + best.BoardingTime() * best.id);
+            Console.WriteLine("Solution Part 1 = " + best.BoardingTime() * best.id);
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            string[] data = _input.Split('\n');
+            List<string> buses = data[1].Split(",").ToList();
+            List<BusLine> busLines = new List<BusLine>();
+            int index = -1;
+            foreach (var s in buses)
+            {
+                index++;
+                if (s == "x") continue;
+                busLines.Add(new BusLine { id = int.Parse(s), goal = index });
+            }
+
+            long solution = 1;
+            long multiplier = 1;
+            foreach (var b in busLines)
+            {
+                solution = Solve(solution, multiplier, b.id, b.goal);
+                multiplier *= b.id;
+            }
+            Console.WriteLine("Solution Part 2 = " + solution);
+        }
+
+        static long Solve(long solution, long multiplier, int id, int index)
+        {
+            while (true)
+            {
+                solution += multiplier;
+                if ((solution + index) % id == 0) break;
+            }
+            return solution;
         }
     }
     public class BusLine
