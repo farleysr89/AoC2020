ad5cab053e5d769f7697cd0d2257407a072ab239
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-16 16:27:47 -0600

    Day 16 Setup

---
+++ Day16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        static void SolvePart1()
+        {
+            string _input = File.ReadAllText("Input.txt");
+        }
+
+        static void SolvePart2()
+        {
+            string _input = File.ReadAllText("Input.txt");
+        }
+    }
+}
