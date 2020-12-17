379530544f39db02e96756fe9a37971e6e41b672
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-17 17:18:52 -0600

    Day 17 Part 1 Complete

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day17
 {
@@ -14,11 +16,65 @@ namespace Day17
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            Dictionary<(int, int, int), char> cubes = new Dictionary<(int, int, int), char>();
+            int x = 0, y = 0, z = 0;
+            foreach (var s in data)
+            {
+                if (s == "") continue;
+                x = 0;
+                foreach (var c in s)
+                {
+                    cubes.Add((x, y, z), c);
+                    x++;
+                }
+                y++;
+            }
+            for (x = -6; x < 14; x++)
+            {
+                for (y = -6; y < 14; y++)
+                {
+                    for (z = -6; z < 7; z++)
+                    {
+                        if (!cubes.ContainsKey((x, y, z))) cubes.Add((x, y, z), '.');
+                    }
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                var newCubes = new Dictionary<(int, int, int), char>(cubes);
+                foreach (var cube in cubes)
+                {
+                    var count = CheckNeighbors(cube.Key, cubes);
+                    if (cube.Value == '#' && (count != 2 && count != 3)) newCubes[cube.Key] = '.';
+                    if (cube.Value == '.' && count == 3) newCubes[cube.Key] = '#';
+                }
+                cubes = new Dictionary<(int, int, int), char>(newCubes);
+            }
+            Console.WriteLine("Total active cubes = " + cubes.Where(c => c.Value == '#').Count());
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+        }
+
+        static int CheckNeighbors((int, int, int) key, Dictionary<(int, int, int), char> cubes)
+        {
+            int count = 0;
+            for (int x = key.Item1 - 1; x <= key.Item1 + 1; x++)
+            {
+                for (int y = key.Item2 - 1; y <= key.Item2 + 1; y++)
+                {
+                    for (int z = key.Item3 - 1; z <= key.Item3 + 1; z++)
+                    {
+                        if ((x, y, z) == key) continue;
+                        if (!cubes.ContainsKey((x, y, z))) continue;
+                        if (cubes[(x, y, z)] == '#') count++;
+                    }
+                }
+            }
+            return count;
         }
     }
 }
