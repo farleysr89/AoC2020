079043d7447247b05433b673d83f3c8abdf799ed
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-29 16:18:11 -0600

    Day 24 Part 1 Complete

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -7,7 +7,7 @@ namespace Day24
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             SolvePart1();
             SolvePart2();
@@ -17,7 +17,28 @@ namespace Day24
         {
             string _input = File.ReadAllText("Input.txt");
             List<string> data = _input.Split('\n').ToList();
-            Console.WriteLine("");
+            Dictionary<(int, int), bool> tiles = new Dictionary<(int, int), bool>();
+            foreach (var s in data)
+            {
+                int x = 0, y = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    var c = s[i];
+                    if (c == 'n' || c == 's')
+                    {
+                        var cc = s[++i];
+                        if (cc == 'e') x++;
+                        if (cc == 'w') x--;
+                        if (c == 'n') y++;
+                        if (c == 's') y--;
+                    }
+                    else if (c == 'w') x -= 2;
+                    else if (c == 'e') x += 2;
+                }
+                if (tiles.ContainsKey((x, y))) tiles[(x, y)] = !tiles[(x, y)];
+                else tiles[(x, y)] = true;
+            }
+            Console.WriteLine("Final count = " + tiles.Where(t => t.Value).Count());
         }
 
         static void SolvePart2()
