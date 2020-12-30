c5180632875e337edb143db852b40bfaa45f263f
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-30 15:23:23 -0600

    Day 25 Setup

---
+++ Day25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        static void SolvePart1()
+        {
+            string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+
+        static void SolvePart2()
+        {
+            string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
